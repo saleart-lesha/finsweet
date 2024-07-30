323016fd62d9579fc17b892040231db80a8c62ed
Aleksey Svishchev <aleksey.svischev01@gmail.com>
2024-07-30 22:36:21 +0300

    refactor: Отрефакторил блок AboutUsAndOurMission

--- src/entities/AboutUsAndOurMission/AboutUsAndOurMission.tsx
+++ src/entities/AboutUsAndOurMission/AboutUsAndOurMission.tsx
@@ -1,36 +1,33 @@
 import { Link } from 'react-router-dom'
-import './scss/AboutUsAndOurMission.css'
 
 const AboutUsAndOurMission = () => {
   return (
-    <section className='AboutUsAndOurMission'>
-      <div className='main_body'>
-        <div className='separation'>
-          <div className='first_block'></div>
-          <div className='second_block'></div>
-          <div className='third_block'></div>
+    <section className='aboutUsAndOurMission'>
+      <div className='aboutUsAndOurMission__separation'>
+        <div className='aboutUsAndOurMission__separation__first__block'></div>
+        <div className='aboutUsAndOurMission__separation__second__block'></div>
+        <div className='aboutUsAndOurMission__separation__third__block'></div>
+      </div>
+      <div className='aboutUsAndOurMission__container'>
+        <div className='aboutUsAndOurMission__card'>
+          <h2>ABOUT US</h2>
+          <h1>We are a community of content writers who share their learnings</h1>
+          <p className='aboutUsAndOurMission__card__description'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </p>
+          <Link to='/' className='aboutUsAndOurMission__reed__more_btn'>
+            {'Read More >'}
+          </Link>
         </div>
-        <div className='container'>
-          <div className='about_us'>
-            <h2>ABOUT US</h2>
-            <h1>We are a community of content writers who share their learnings</h1>
-            <p className='description_text'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua.
-            </p>
-            <Link to='/' className='reed_more'>
-              {'Read More >'}
-            </Link>
-          </div>
-          <div className='our_mission'>
-            <h2>OUR MISSION</h2>
-            <h1>Creating valuable content for creatives all around the world</h1>
-            <p className='description_text'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
+        <div className='aboutUsAndOurMission__card'>
+          <h2>OUR MISSION</h2>
+          <h1>Creating valuable content for creatives all around the world</h1>
+          <p className='aboutUsAndOurMission__card__description'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
         </div>
       </div>
     </section>
