ed6c674e3a4aa8db91dbec2a71058e11b79ad959
Aleksey Svishchev <aleksey.svischev01@gmail.com>
2024-07-31 20:31:44 +0300

    refactor: Отрефакторил блок weStarted

--- src/entities/WeStarted/WeStarted.tsx
+++ src/entities/WeStarted/WeStarted.tsx
@@ -4,23 +4,19 @@ import Button from '../../shared/subscribeBtn/Button'
 
 const WeStarted = () => {
   return (
-    <section className='WeStarted'>
-      <div className='main_body'>
-        <div className='container'>
-          <img src={bgimage} alt=''></img>
-          <div className='content_container'>
-            <div className='content'>
-              <div className='we_started'>
-                <h2>WHY WE STARTED </h2>
-                <h1>It started out as a simple idea and evolved into our passion</h1>
-                <p className='description_text'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip.
-                </p>
-                <Button color='yellow' text='Discover our story >' />
-              </div>
-            </div>
+    <section className='weStarted'>
+      <div className='weStarted__main__body'>
+        <img src={bgimage} alt=''></img>
+        <div className='weStarted__container'>
+          <div className='weStarted__container__content'>
+            <h2>WHY WE STARTED </h2>
+            <h1>It started out as a simple idea and evolved into our passion</h1>
+            <p className='weStarted__container__description'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip.
+            </p>
+            <Button color='yellow' text='Discover our story >' />
           </div>
         </div>
       </div>
