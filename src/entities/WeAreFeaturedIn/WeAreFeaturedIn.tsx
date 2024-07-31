6c06ea12f226cb2aaa17e1bf9f90117a3f73863d
Aleksey Svishchev <aleksey.svischev01@gmail.com>
2024-07-31 21:04:02 +0300

    refactor: Отрефакторил блок weAreFeaturedIn

--- src/entities/WeAreFeaturedIn/WeAreFeaturedIn.tsx
+++ src/entities/WeAreFeaturedIn/WeAreFeaturedIn.tsx
@@ -1,4 +1,3 @@
-import './scss/WeAreFeaturedIn.css'
 import logo1 from './../../app/img/homePage/WeAreFeaturedIn/Logo 1.svg'
 import logo2 from './../../app/img/homePage/WeAreFeaturedIn/Logo 2.svg'
 import logo3 from './../../app/img/homePage/WeAreFeaturedIn/Logo 3.svg'
@@ -7,18 +6,16 @@ import logo5 from './../../app/img/homePage/WeAreFeaturedIn/Logo 5.svg'
 
 const WeAreFeaturedIn = () => {
   return (
-    <section className='WeAreFeaturedIn'>
-      <div className='main_body'>
-        <div className='text'>
-          <p className='firs_text'>We are</p>
-          <p className='second_text'>Featured in</p>
-        </div>
-        <img src={logo1} alt='' />
-        <img src={logo2} alt='' />
-        <img src={logo3} alt='' />
-        <img src={logo4} alt='' />
-        <img src={logo5} alt='' />
+    <section className='weAreFeaturedIn'>
+      <div className='weAreFeaturedIn__container'>
+        <p className='weAreFeaturedIn__container__first__text'>We are</p>
+        <p className='weAreFeaturedIn__container__second__text'>Featured in</p>
       </div>
+      <img src={logo1} alt='' />
+      <img src={logo2} alt='' />
+      <img src={logo3} alt='' />
+      <img src={logo4} alt='' />
+      <img src={logo5} alt='' />
     </section>
   )
 }
